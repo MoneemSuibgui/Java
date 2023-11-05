ab13ca919bb4c6107d47ed8180505472cdfead36
Moneem1 <125930635+Moneem1@users.noreply.github.com>
2023-11-05 18:23:11 +0100

    first Java Program Assignment

---
+++ Java_Fundamentals/First_Java_Program/Test.java
@@ -0,0 +1,9 @@
+public class Test{
+    // main method (entry point)
+    public static void main(String[] args){
+        //   print my name, age, and hometown on different lines
+        System.out.println("My name is Moneem Suibgui");
+        System.out.println("I am 32 years old");
+        System.out.println("My hometown is Kasserine,Tunisia");
+    }
+}
