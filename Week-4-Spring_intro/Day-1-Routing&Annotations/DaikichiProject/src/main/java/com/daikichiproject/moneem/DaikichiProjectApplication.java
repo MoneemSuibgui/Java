c042325a6111a6bb865c3a6508b886fdd805b024
Moneem1 <125930635+Moneem1@users.noreply.github.com>
2023-12-24 18:47:01 +0100

    Create a Spring Boot application using Spring Tool Suite

---
+++ Week-4-Spring_intro/Day-1-Routing&Annotations/DaikichiProject/src/main/java/com/daikichiproject/moneem/DaikichiProjectApplication.java
@@ -0,0 +1,13 @@
+package com.daikichiproject.moneem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaikichiProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaikichiProjectApplication.class, args);
+	}
+
+}
