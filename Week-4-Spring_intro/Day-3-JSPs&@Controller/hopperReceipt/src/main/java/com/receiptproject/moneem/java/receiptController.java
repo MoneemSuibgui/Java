c89677953d13fbd5f4b243e7e228c25624af932b
Moneem1 <125930635+Moneem1@users.noreply.github.com>
2023-12-26 20:42:10 +0100

    Render data from Controller

---
+++ Week-4-Spring_intro/Day-3-JSPs&@Controller/hopperReceipt/src/main/java/com/receiptproject/moneem/java/receiptController.java
@@ -0,0 +1,27 @@
+package com.receiptproject.moneem.java;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class receiptController {
+	
+	@RequestMapping("/")
+	public String showReceipt(Model model) {
+		String customerName="Grace Hopper";
+		String itemName="Copper wire";
+		double price=5.25;
+		String Description="Metal strips .Also an illustration of nanoseconds";
+		String vendor="Little Corner Store";
+		
+		// To pass data to our view can be as simple as injecting a Model object to our method:
+		model.addAttribute("customerfullName",customerName);
+		model.addAttribute("itemName", itemName);
+		model.addAttribute("price", price);
+		model.addAttribute("description",Description);
+		model.addAttribute("vendor", vendor);
+		return "index.jsp";
+	}
+
+}
