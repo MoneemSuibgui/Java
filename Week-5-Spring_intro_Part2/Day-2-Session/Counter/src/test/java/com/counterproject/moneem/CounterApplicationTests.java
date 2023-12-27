d6253a512a128a8ab4d90a0ef7ee4fa779ebc4b8
Moneem1 <125930635+Moneem1@users.noreply.github.com>
2023-12-27 21:53:35 +0100

    Using session to store data

---
+++ Week-5-Spring_intro_Part2/Day-2-Session/Counter/src/test/java/com/counterproject/moneem/CounterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.counterproject.moneem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CounterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
