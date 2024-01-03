734e606cc2bebbc2e98bdd6d67548f548cb2e65b
Moneem1 <125930635+Moneem1@users.noreply.github.com>
2024-01-03 20:07:12 +0100

    Create a service layer to perform business logic in my app

---
+++ Week-6-Spring_MVC/Day-2-MVC_Layers_Spring/Books_API/src/main/java/com/book_api/moneem/services/BookService.java
@@ -0,0 +1,67 @@
+package com.book_api.moneem.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.book_api.moneem.models.Book;
+import com.book_api.moneem.repositories.BookRepository;
+
+@Service
+public class BookService {
+	
+	private final BookRepository bookRepo;
+	
+	public BookService(BookRepository bookRepo) {
+		this.bookRepo=bookRepo;
+	}
+	
+	// return all the books
+	public List<Book> allBooks(){
+		return bookRepo.findAll();
+	}
+	
+	// create one book
+	public Book createBook(Book book) {
+		return bookRepo.save(book);
+	}
+	
+	// update one book
+	public Book updateBook(Book book) {
+		return bookRepo.save(book);
+	}
+	
+	// retrieve one book
+	public Book findBook(Long id) {
+		Optional<Book> optionalBook=bookRepo.findById(id);
+		if(optionalBook.isPresent()) {
+			return optionalBook.get();
+		}else {
+			return null;
+		}
+	}
+	
+	// delete one book
+	public String deleteBook(Long id) {
+		Optional<Book> optinalBook=bookRepo.findById(id);
+		if(optinalBook.isPresent()) {
+			bookRepo.deleteById(id);
+			return "Book Successfuly deleted ";
+		}else {
+			return "This book is not in the database";
+		}
+	}
+	
+	// we could delete one book other way just passing the id and the method return nothing
+	
+	//public void deleteOneBook(Long id) {
+	//	 bookRepo.deleteById(id);
+	//}
+	
+	
+	
+	
+	
+
+}
