8ef1a5bb2f03885cf2711aa088cd84e0d11d3182
Moneem1 <125930635+Moneem1@users.noreply.github.com>
2024-02-12 23:10:52 +0100

    Authentication & Authorization using Spring Security

---
+++ Week-10-Spring_Security/SpringSecurity/src/test/java/com/hello_doctor/java/SpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hello_doctor.java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
