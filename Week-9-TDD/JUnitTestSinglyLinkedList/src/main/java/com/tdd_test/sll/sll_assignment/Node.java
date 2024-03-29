bf2a81e33d597d97c9c145c071117f7f3cd5dee4
Moneem1 <125930635+Moneem1@users.noreply.github.com>
2024-03-29 20:38:25 +0100

    Create Node class

---
+++ Week-9-TDD/JUnitTestSinglyLinkedList/src/main/java/com/tdd_test/sll/sll_assignment/Node.java
@@ -0,0 +1,13 @@
+package com.tdd_test.sll.sll_assignment;
+
+public class Node {
+
+	// member variables
+	public int value;
+	public Node next;
+	
+	// create Node object that accept given value of type integer
+	public Node(int value) {
+		this.value=value;
+	}
+}
